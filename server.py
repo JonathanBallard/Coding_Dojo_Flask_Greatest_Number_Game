8cff93daf35c249586697f89fdd56f5569c86870
Jonathan Ballard <asgardschosen@gmail.com>
2020-01-21 13:30:31 -0800

    initial commit

---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
+# our index route will handle rendering our form
+
+
+
+
+@app.route('/')
+def index():
+    if 'randomNum' in session:
+        pass
+        # print('Number Is: ' + str(session['randomNum']))
+    else:
+        session['randomNum'] = random.randint(1,100)
+        # print('Number Created: ' + str(session['randomNum']))
+    
+
+    if 'guess' in session:
+        pass
+        # print('Guess Found, and Is: ' + str(session['guess']))
+    else:
+        if request.form.get('guess'):
+            session['guess'] = int(request.form['guessedNum'])
+            # print('Guess Is: ' + str(session['guess']))
+        else:
+            session['guess'] = -1
+            # print('Guess DEFAULT: ' + str(session['guess']))
+
+    return render_template("index.html")
+
+
+@app.route('/destroy_session')
+def destroy():
+
+    session.clear()
+    return redirect("/")
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    session['guess'] = int(request.form['guessedNum'])
+    session['randomNum'] = int(session['randomNum'])
+    return redirect("/")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
